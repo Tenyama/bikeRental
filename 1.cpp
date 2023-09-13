ff898ddc557e009bcf27a4c9af02cdb0176d3868
nmquan1 <nmquanrmit@gmail.com>
2023-09-13 17:31:47 +0700

    yea

--- 1.cpp
+++ 1.cpp
@@ -2,12 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "Hello the gioi";
-    cout << "update lan 1 nha";
-    cout << "update lan 2 nha";
-    cout << "update lan 3, dit me thang son";
-    cout << "update lan 4 bo may test cai nay phat";
-    cout << "update lan 5";
+
     return 0;
     //add commit push
 }
