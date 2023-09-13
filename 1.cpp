1349c40641b8d578b4ca8da0e229aa5434caf779
nmquan1 <nmquanrmit@gmail.com>
2023-09-13 17:22:08 +0700

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,6 +5,7 @@ int main(){
     cout << "Hello the gioi";
     cout << "update lan 1 nha";
     cout << "update lan 2 nha";
+    cout << "update lan 3, dit me thang son";
     return 0;
     //add commit push
 }
